72eaf0cd1113f1dde843f0ea5fabbc55695de174
JiYu <eunjun1018@gmail.com>
2025-04-08 00:41:34 +0900

    deepEquals 비교함수 구현

--- src/@lib/equalities/deepEquals.ts
+++ src/@lib/equalities/deepEquals.ts
@@ -1,53 +1,53 @@
-// export function deepEquals<T>(objA: T, objB: T): boolean {
-//   // 참조가 같을 떄 >>> 기본 타입이거나 null인 경우
-//   if (objA === objB) {
-//     return true;
-//   }
+export function deepEquals<T>(objA: T, objB: T): boolean {
+  // 참조가 같을 떄 >>> 기본 타입이거나 null인 경우
+  if (objA === objB) {
+    return true;
+  }
 
-//   // 둘 중 하나라도 객체일 경우 >>> 기본 타입이거나 null인 경우
-//   if (
-//     typeof objA !== "object" ||
-//     objA === null ||
-//     typeof objB !== "object" ||
-//     typeof objB === null
-//   ) {
-//     return false;
-//   }
+  // 둘 중 하나라도 객체일 경우 >>> 기본 타입이거나 null인 경우
+  if (
+    typeof objA !== "object" ||
+    objA === null ||
+    typeof objB !== "object" ||
+    objB === null
+  ) {
+    return false;
+  }
 
-//   // 배열여부 확인인
-//   const isArrayA = Array.isArray(objA);
-//   const isArrayB = Array.isArray(objB);
-//   if (isArrayA !== isArrayB) {
-//     return false;
-//   }
+  // 배열여부 확인
+  const isArrayA = Array.isArray(objA);
+  const isArrayB = Array.isArray(objB);
+  if (isArrayA !== isArrayB) {
+    return false;
+  }
 
-//   if (isArrayA && isArrayB) {
-//     // 배열일 경우, length가 다르면 false
-//     if ((objA as any[]).length !== (objB as any[]).length) {
-//       return false;
-//     }
+  if (isArrayA && isArrayB) {
+    const arrA = objA as unknown[];
+    const arrB = objB as unknown[];
 
-//     // 배열열요소들을 재귀적으로 deepEquals
-//     return (objA as any[]).every((item, i) =>
-//       deepEquals(item, (objB as any[])[i]),
-//     );
-//   }
+    // 배열일 경우, 길이가 다르면 false
+    if (arrA.length !== arrB.length) {
+      return false;
+    }
 
-//   const keysA = Object.keys(objA as object);
-//   const keysB = Object.keys(objB as object);
+    return arrA.every((item, i) => deepEquals(item, arrB[i]));
+  }
 
-//   // 객체의 키 개수가 다른 경우우
-//   if (keysA.length !== keysB.length) {
-//     return false;
-//   }
+  const keysA = Object.keys(objA as object);
+  const keysB = Object.keys(objB as object);
 
-//   // 각각에 deepEquals 재귀적으로 호출
-//   for (const key of keysA) {
-//     if (!Object.prototype.hasOwnProperty.call(objB, key)) return false;
-//     const a = (objA as any)[key];
-//     const b = (objB as any)[key];
-//     if (!deepEquals(a, b)) return false;
-//   }
+  // 객체의 키 개수가 다른 경우우
+  if (keysA.length !== keysB.length) {
+    return false;
+  }
 
-//   return true;
-// }
+  // 각각에 deepEquals 재귀적으로 호출
+  for (const key of keysA) {
+    if (!Object.prototype.hasOwnProperty.call(objB, key)) return false;
+    const a = (objA as Record<string, unknown>)[key];
+    const b = (objB as Record<string, unknown>)[key];
+    if (!deepEquals(a, b)) return false;
+  }
+
+  return true;
+}
