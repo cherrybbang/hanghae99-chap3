c8194e51a4fba07d0432d3610fbda9e6c8e7504c
JiYu <eunjun1018@gmail.com>
2025-04-09 00:31:31 +0900

    hooks, hoc 구현

--- src/@lib/hocs/memo.ts
+++ src/@lib/hocs/memo.ts
@@ -1,10 +1,22 @@
-/* eslint-disable @typescript-eslint/no-unused-vars */
 import { shallowEquals } from "../equalities";
-import { ComponentType } from "react";
+import React, { ComponentType } from "react";
+import { useRef } from "../hooks/useRef";
 
 export function memo<P extends object>(
   Component: ComponentType<P>,
   _equals = shallowEquals,
 ) {
-  return Component;
+  const MemoComponent = (props: P) => {
+    const memoProps = useRef<P | null>(null);
+
+    if (memoProps.current === null) {
+      memoProps.current = props;
+      return React.createElement(Component, props);
+    }
+    if (!_equals(memoProps.current, props)) {
+      memoProps.current = props;
+      return React.createElement(Component, props);
+    }
+  };
+  return MemoComponent; // React component names must start with an uppercase letter.
 }
