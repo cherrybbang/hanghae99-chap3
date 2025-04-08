312f37c07763174221bacf268202615d4e48e7f1
JiYu <eunjun1018@gmail.com>
2025-04-09 00:40:14 +0900

    hooks, hoc 구현

--- src/@lib/hooks/useCallback.ts
+++ src/@lib/hooks/useCallback.ts
@@ -1,4 +1,5 @@
 /* eslint-disable @typescript-eslint/no-unsafe-function-type */
+/* eslint-disable react-hooks/exhaustive-deps */
 import { DependencyList } from "react";
 import { useMemo } from "../hooks/useMemo";
 
@@ -10,3 +11,4 @@ export function useCallback<T extends Function>(
 }
 
 // useCallback은 함수 자체를 저장한다.
+// dependency는 hook 안에서 사용하는 외부값, 해당 값이 바뀌면 hook을 다시 동작하게 만든다.
